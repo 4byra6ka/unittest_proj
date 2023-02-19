c12b838a12475c65a6aed6550acdcfa6ce0d02fb
GAU <gau@1jz.ru>
2023-02-19 22:56:13 +0300

    TASK 5.2 create and TDD unittest in test_dicts.py

---
+++ tests/test_dicts.py
@@ -0,0 +1,10 @@
+import unittest
+from utils import dicts
+
+class TestDicts(unittest.TestCase):
+    """Unittest на основе TDD"""
+    def test_get_val(self):
+        self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs"), "mercurial")
+        self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs", "git"), "mercurial")
+        self.assertEqual(dicts.get_val({}, "vcs", "git"), "git")
+        self.assertEqual(dicts.get_val({}, "vcs", "bazaar"), "bazaar")
